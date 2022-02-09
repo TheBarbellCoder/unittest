036fa2c9c9465ba3d35c4f91ec310a3eb00262d8
Avinash Chikkadlur Ravi Shankar <avinash.ravishankar@outlook.com>
2022-02-09 07:41:53 +0100

    updated to class based unit-testing and reduced multiple defines

--- README.md
+++ README.md
@@ -8,19 +8,10 @@ The project defines the following basic *MACROS* to construct test cases.
 
 | MACROS | Description |
 | :---- | :---- |
-| `TEST(TestSuite, TestName)` | Declares test cases. |
-| `BEGIN_TEST(TestSuite, TestName)` | Construct a testing environment. |
-| `END_TEST` | Tear-down the test case. |
-| `RUN_TEST(TestSuite, TestName)` | Calls the test case to execute. |
-| `EXPECT_EQ(arg1, arg2)` | Checks if two arguments are equal. |
-
-### Additional Macros
-
-| MACROS | Description |
-| :---- | :---- |
-|`INIT_TEST_SUMMARY` | Initializates variables to store the number of passed and failed test cases. <br> (Mandatory) |
-| `RUN_TEST_SUMMARY` | Calculates and prints the total number of passed and failed test cases. <br> (Optional)|
-| `TEST_STATUS` | `0` if all tests pass. Else, `-1`. <br> (For use when running with CTest.) |
+| `DefineTest(TestSuite, TestName)` | Define and Declare test cases |
+| `RegisterTest(TestSuite, TestName)` | Register test cases to be run |
+| `RunTests` | Executes all registerd tests |
+| `ExpectEQ(arg1, arg2)` | Checks if two arguments are equal |
 
 ## Sample Usage
 
@@ -30,56 +21,38 @@ Here's how you can use UnitTest in your project.
 `git submodule add git@gitlab.com:AvinashRavishankar/unittest.git
 ./test/unittest`
 
-- Create a header file to declare your tests.
+- Create a source file to declare and define your test cases.
 ```cpp
-//sample_test.hpp
+//sample_test.cpp
 
-TEST(Suite1, Name1)
-TEST(Suite2, Name2)
+DefineTest(Suite1, Name1)
+{
+   // Define your test case here
+}
+DefineTest(Suite2, Name2)
+{
+   // Define your test case here
+}
 ...
 ```
 
-- Create a source file to define all your test cases.
-```cpp
-// sample_test.cpp
-
-BEGIN_TEST(Suite1, Name1){
-   // Initializations
-   // Call the function under test
-   EXPECT_EQ(valueReturned, valueExpected)
-}END_TEST
-
-BEGIN_TEST(Suite2, Name2){
-   // Initializations
-   // Call the function under test
-   EXPECT_EQ(valueReturned, valueExpected)
-}END_TEST
-
-...
-```
-- Create a source file to run all tests.
+- Create a source file to register and run all tests.
 ```cpp
 // test_main.cpp
 
 #include "unittest/unittest.hpp"
 #include "sample_test.hpp"
 
-INIT_TEST_SUMMARY // Should be defined in the main test source.
-
 int main(){
 
-   RUN_TEST(Suite1, Name1)
-   RUN_TEST(Suite2, Name2)
+   RegisterTest(Suite1, Name1)
+   RegisterTest(Suite2, Name2)
+   ...
    ...
 
-   RUN_TEST_SUMMARY // Prints test statistics (optional)
+   RunTests()
 
-   return TEST_STATUS; /** TEST_STATUS should be used when running tests with
-                         CTest. Otherwise, the progam may return -1 on
-                         successful execution.
-
-                         Use `return 0` otherwise.
-                       */
+   return 0;
 }
 ```
 ## Thread Safety
@@ -87,8 +60,4 @@ int main(){
 UnitTest allows *forking* test cases to run them in separate threads. This
 way, when a test case crashes, it dosen't bring down the entire program.
 
-Forking is disabled by default. To enable it, simply append `-DFORK` to your
-compiler options. No  more changes! :relaxed:
-
-
-
+Forking is disabled by default and is currently available to _unix-like_ OS. To enable it, simply define `FORK` in your main test source file or append `-DFORK` to your compile command! :relaxed:
--- unittest.hpp
+++ unittest.hpp
@@ -1,113 +1,197 @@
 #ifndef TEST_INCLUDED
 #define TEST_INCLUDED
 
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <iostream>
-#include <iomanip>
-#include <cstdlib>
+#include <sstream>
+#include <list>
 
-struct TestSummary{
-   static int True;
-   static int False;
-};
+class Log
+{
+    std::stringstream _stream;
+    std::stringstream _sFailed;
+    std::stringstream _sPassed;
 
-#define INIT_TEST_SUMMARY                                                     \
-   int TestSummary::True = 0;                                                 \
-   int TestSummary::False = 0;
+    public:
+    Log(const char* module, const char* test, bool state)
+    {
+        std::stringstream sUnit;
+        sUnit.flags(std::ios_base::left);
+        sUnit << "( " << module << " ) " << test << "   ";
 
-#define EXPECT_EQ(arg1, arg2) isTrue &= (arg1 == arg2);
+        _sFailed.flags(std::ios_base::right);
+        _sFailed.fill('.');
+        _sFailed.width(50);
 
-#define TEST_STATUS (TestSummary::False > 0) ? -1 : 0;
+        _sPassed.flags(std::ios_base::right);
+        _sPassed.fill('.');
+        _sPassed.width(50);
 
-#define TEST(TestSuite, TestName) bool test_##TestSuite##_##TestName(void);
-
-#ifdef FORK
-
-#define BEGIN_TEST(TestSuite, TestName)                                       \
-   bool test_##TestSuite##_##TestName(void)                                   \
-{                                                                             \
-      bool ret;                                                               \
-      pid_t pid = fork();                                                     \
-                                                                              \
-      if(pid < 0)                                                             \
-      {                                                                       \
-        std::cout                                                             \
-        << "\n" << "In function " << __func__ << ": \n"                       \
-        << "\x1b[38;5;160mFailed to create a child process!!\x1b[0m"          \
-        << std::endl;                                                         \
-        ret = false;                                                          \
-      }                                                                       \
-      if(pid > 0)                                                             \
-      {                                                                       \
-        int status;                                                           \
-        waitpid(pid, &status, 0);                                             \
-        if(WIFEXITED(status))                                                 \
-        {                                                                     \
-          ret = (WEXITSTATUS(status) == EXIT_SUCCESS) ? true : false;         \
-        }                                                                     \
-        else                                                                  \
-        {                                                                     \
-          std::cout                                                           \
-          << "\n" << "In function " << __func__ << ": \n"                     \
-          << "\x1b[38;5;160mFailed to exit normally!!\x1b[0m"                 \
-          << std::endl;                                                       \
-          ret = false;                                                        \
-        }                                                                     \
-      }                                                                       \
-      if(pid == 0)                                                            \
-      {                                                                       \
-        bool isTrue{true};
-
-#define END_TEST                                                              \
-        exit(isTrue ? EXIT_SUCCESS : !EXIT_SUCCESS);                          \
-      }                                                                       \
-      return ret;                                                             \
-}
-
+#ifdef __unix__
+        _sFailed << "\x1b[38;5;160m   FAILED \x1b[0m \n";
+        _sPassed << "\x1b[38;5;40m   OK \x1b[0m \n";
 #else
-
-#define BEGIN_TEST(module, name)                                              \
-   bool test_##module##_##name(void)                                          \
-{                                                                             \
-   bool isTrue{true};
-
-#define END_TEST                                                              \
-   return isTrue;                                                             \
-}
-
+        _sFailed << "   FAILED \n";
+        _sPassed << "   OK \n";
 #endif
 
-#define RUN_TEST(module, name)                                                \
-{                                                                             \
-                                                                              \
-   bool ret = test_##module##_##name();                                       \
-   std::cout << std::left << std::setfill('-')                                \
-   << std::setw(50) << #module " -> " #name " ";                              \
-                                                                              \
-   if(ret)                                                                    \
-   {                                                                          \
-      TestSummary::True += 1;                                                 \
-      std::cout << std::setw(10)                                              \
-      << std::left << "\x1b[38;5;40m   OK \x1b[0m" << "\n" << std::endl;      \
-   }                                                                          \
-   else                                                                       \
-   {                                                                          \
-      TestSummary::False += 1;                                                \
-      std::cout << std::setw(10)                                              \
-      << std::left << "\x1b[38;5;160m   FAILED \x1b[0m" << "\n" << std::endl; \
-   }                                                                          \
-}
+        if(state) { _stream << sUnit.str() + _sPassed.str(); }
+        else { _stream << sUnit.str() << _sFailed.str(); }
+    }
 
-#define RUN_TEST_SUMMARY                                                      \
-   std::cout << std::setw(80) << std::left << std::setfill('-') << "\n"       \
-   << std::endl;                                                              \
-   std::cout << "Tests Passed : " << "\x1b[38;5;40m" << TestSummary::True     \
-   << "\x1b[0m" << std::endl;                                                 \
+    ~Log() { std::cerr << _stream.str() << std::endl; }
+};
+
+class Summary
+{
+    std::stringstream _sBorder;
+    std::stringstream _sTotal;
+    std::stringstream _sPassed;
+    std::stringstream _sFailed;
+
+    int total;
+    public:
+        Summary(const int& passed, const int& failed):
+            total(passed + failed)
+        {
+            _sBorder.fill('=');
+            _sBorder.width(80);
+            _sBorder << "\n";
+
+            _sPassed << "Tests Passed : " << "\x1b[38;5;40m"
+                << passed << "\x1b[0m";
+            _sFailed << "Tests Failed : " << "\x1b[38;5;160m"
+                << failed << "\x1b[0m";
+
+            _sTotal << "Number of tests: ";
+        }
+
+        ~Summary()
+        {
+            std::cerr << "\n" << _sBorder.str() <<  _sTotal.str()
+                << total << "\n" << _sPassed.str() << "\n" << _sFailed.str()
+                << "\n" << _sBorder.str() << std::endl;
+        }
+};
+
+class UnitTest
+{
+    UnitTest() {}
+
+    protected:
+
+        UnitTest(const char* module, const char* test):
+            moduleName(module), testName(test) {}
+
+    public:
+
+        const char* moduleName;
+        const char* testName;
+        bool isTrue{true};
+        std::list<UnitTest*> testList;
+
+        static UnitTest& getInstance()
+        {
+            static UnitTest uTest;
+            return uTest;
+        }
+
+        void runTests()
+        {
+            static int passed{0};
+            static int failed{0};
+
+            if(testList.empty())
+            {
+                std::cout << "No tests registered!!" << std::endl;
+                return;
+            }
+
+            for(UnitTest* x: testList)
+            {
+#if  defined(__unix__) && defined(FORK)
+
+                pid_t pid = vfork();
+
+                if(pid < 0)
+                {
+                    std::cerr << "\n" << "In function " << __func__ << ": /n"
+                        << "\x1b[38;5;160mFailed to create a child process!!\x1b[0m"
+                        << std::endl;
+                }
+
+                if(pid > 0)
+                {
+                    int status;
+                    waitpid(pid, &status, WCONTINUED);
+                    if(WIFEXITED(status) == 0)
+                    {
+                        std::cerr << "\n" << "In function " << __func__ << ": \n"
+                            << "\x1b[38;5;160mFailed with error code " <<
+                            WEXITSTATUS(status) << "\x1b[0m" << std::endl;
+                    }
+                }
+
+                if(pid == 0)
+                {
+                    x->runFunc();
+                    x->isTrue ? ++passed : ++failed;
+                    Log(x->moduleName, x->testName, x->isTrue);
+                    _exit(0);
+                }
+#else
+                x->runFunc();
+                x->isTrue ? ++passed : ++failed;
+                Log(x->moduleName, x->testName, x->isTrue);
+
+#endif // __unix__ && FORK
+            }
+            Summary(passed, failed);
+        }
+
+        template<typename T>
+        inline bool expectEQ(const T& arg1, const T& arg2)
+        {
+            bool isTrue{arg1 == arg2};
+            if(!isTrue)
+            {
+                std::cout << "Actual: " << arg1 << " vs. Expected: " << arg2
+                    << std::endl;
+            }
+            return isTrue;
+        }
+
+        virtual void runFunc() {}
+        virtual ~UnitTest() {}
+};
+
+#define ExpectEQ(arg1, arg2)                                                 \
+    isTrue &= expectEQ(arg1, arg2);
+
+#define DefineTest(Module, TestName)                                          \
+    class Test_##Module##_##TestName: public UnitTest                         \
+{                                                                             \
+    Test_##Module##_##TestName(): UnitTest(#Module, #TestName){}              \
                                                                               \
-   std::cout << "Tests Failed : " << "\x1b[38;5;160m" << TestSummary::False   \
-   << "\x1b[0m" << std::endl;                                                 \
-   std::cout << std::left << std::setfill('-') << std::setw(80) << std::endl;
+    public:                                                                   \
+                                                                              \
+    ~Test_##Module##_##TestName() {}                                          \
+                                                                              \
+    static Test_##Module##_##TestName& getInstance()                          \
+    {                                                                         \
+        static Test_##Module##_##TestName newTestUnit;                        \
+        return newTestUnit;                                                   \
+    }                                                                         \
+    void runFunc() override;                                                  \
+};                                                                            \
+                                                                              \
+void Test_##Module##_##TestName::runFunc()
+
+#define RegisterTest(Module, TestName)                                        \
+    UnitTest::getInstance().testList.push_back(&(Test_##Module##_##TestName::getInstance()));
+
+#define RunTests()                                                            \
+    UnitTest::getInstance().runTests();
 
 #endif
